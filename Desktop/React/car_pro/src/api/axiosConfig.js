7cf41245ed32fa1896d2d928c4136e9e60f83248
Sanjay006-SM <smsanjaydmt@gmail.com>
2025-12-19 10:24:00 +0530

    Update axiosConfig.js

--- Desktop/React/car_pro/src/api/axiosConfig.js
+++ Desktop/React/car_pro/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://full-stack-backend-eoon.onrender.com',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
